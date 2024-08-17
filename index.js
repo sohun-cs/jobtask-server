7c80a832d16cc5d2971c06838cac04e21e557901
sohun-cs <asaduzzamansohun5@gmail.com>
2024-08-17 17:37:26 +0600

    connected to the mongodb server

--- index.js
+++ index.js
@@ -4,6 +4,52 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+// app.use(cors({
+//     origin: ["http://localhost:5173",
+//     ]
+// }));
+// app.use(express.json());
+
+
+// products
+// J22Uvxd0QdGYfaSH
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_PRODUCTS}:${process.env.DB_PASS}@cluster0.k7dzav4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        // const productsCollection = client.db('productsDB').collection('products');
+
+
+        // app.get('/products', async (req, res) => {
+        //     const result = await productsCollection.find().toArray();
+        //     res.send(result);
+        // });
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('JobTask Server is running.');
 });
